8608314106b0b7265c938621819db2089842c601
David Oliveira <dedepco@gmail.com>
2025-10-05 10:49:54 -0300

    [TASK-001] feat: Adiciona configuração do Jest e scripts de teste ao package.json

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  clearMocks: true,
+  verbose: true,
+  coverageDirectory: 'coverage',
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+};
+
+
